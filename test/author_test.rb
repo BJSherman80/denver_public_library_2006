6a6d2056caf09baca65c234f2a275b5ffb0899ed
Brett Turing <bjsherman80@gmail.com>
2020-07-22 13:36:43 -0600

    Add write multiple books test

--- test/author_test.rb
+++ test/author_test.rb
@@ -24,6 +24,25 @@ class AuthorTest < Minitest::Test
     assert_equal [], charlotte_bronte.books
   end
 
+  def test_author_can_write_a_book
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+
+    assert_equal jane_eyre, charlotte_bronte.books
+
+  end
+
+  def test_author_can_write_multiple_books
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    villette = charlotte_bronte.write("Villette", "1853")
+    jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+
+    assert_equal villette, jane_eyre, charlotte_bronte.books
+  end
+
+    
+
+
 
 
 
