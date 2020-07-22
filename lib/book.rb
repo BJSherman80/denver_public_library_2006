c7bb5d3336f3f12bbb0f341196a5560eacce5ab9
Brett Turing <bjsherman80@gmail.com>
2020-07-22 12:47:40 -0600

    Created book class

---
+++ lib/book.rb
@@ -0,0 +1,19 @@
+require 'pry'
+
+class Book
+  attr_reader :title, :author
+  def initialize(attributes)
+    @title = attributes[:title]
+    @author_first_name = attributes[:author_first_name]
+    @author_last_name = attributes[:author_last_name]
+
+  end
+
+  def author
+
+
+  end
+
+
+
+end
