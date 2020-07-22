8131396c4bd11ea1e1e49d21f8c8decf087093cd
Brett Turing <bjsherman80@gmail.com>
2020-07-22 13:05:24 -0600

    Finished book class

--- lib/book.rb
+++ lib/book.rb
@@ -1,17 +1,21 @@
 require 'pry'
 
 class Book
-  attr_reader :title, :author
+  attr_reader :title, :author, :publication_date
   def initialize(attributes)
     @title = attributes[:title]
     @author_first_name = attributes[:author_first_name]
     @author_last_name = attributes[:author_last_name]
+    @publication_date = attributes[:publication_date]
 
   end
 
   def author
+    @author_first_name + " " + @author_last_name
+  end
 
-
+  def publication_year
+    @publication_date.split.last
   end
 
 
